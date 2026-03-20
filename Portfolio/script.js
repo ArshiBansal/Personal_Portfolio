437d4414f0faa8f7f229006b0c3ce46e23a6df84
Pheonix <150040105+ArshiBansal@users.noreply.github.com>
2026-03-20 22:24:15 +0530

    fix

--- Portfolio/script.js
+++ Portfolio/script.js
@@ -48,26 +48,26 @@ window.addEventListener("load", revealOnScroll);
 PROJECT CARD TILT
 ========================= */
 
-const cards = document.querySelectorAll(".project-card");
+// const cards = document.querySelectorAll(".project-card");
 
-cards.forEach((card) => {
-  card.addEventListener("mousemove", (e) => {
-    const rect = card.getBoundingClientRect();
+// cards.forEach((card) => {
+//   card.addEventListener("mousemove", (e) => {
+//     const rect = card.getBoundingClientRect();
 
-    const x = e.clientX - rect.left;
-    const y = e.clientY - rect.top;
+//     const x = e.clientX - rect.left;
+//     const y = e.clientY - rect.top;
 
-    const rotateX = -(y - rect.height / 2) / 25;
-    const rotateY = (x - rect.width / 2) / 25;
+//     const rotateX = -(y - rect.height / 2) / 25;
+//     const rotateY = (x - rect.width / 2) / 25;
 
-    card.style.transform = `perspective(1000px) rotateX(${rotateX}deg) rotateY(${rotateY}deg) translateY(-7px)`;
-  });
+//     card.style.transform = `perspective(1000px) rotateX(${rotateX}deg) rotateY(${rotateY}deg) translateY(-7px)`;
+//   });
 
-  card.addEventListener("mouseleave", () => {
-    card.style.transform =
-      "perspective(1000px) rotateX(0deg) rotateY(0deg) translateY(0)";
-  });
-});
+//   card.addEventListener("mouseleave", () => {
+//     card.style.transform =
+//       "perspective(1000px) rotateX(0deg) rotateY(0deg) translateY(0)";
+//   });
+// });
 
 /* =========================
 PARTICLE NETWORK BACKGROUND
